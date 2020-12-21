42fdb03dabfa684ce98f341c9b68f23ca8a95abe
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-12-21 14:32:31 -0500

    sim set up with null pointer exception

--- src/GUIPanel.java
+++ src/GUIPanel.java
@@ -1,7 +1,57 @@
-package src;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
-public class GUIPanel extends JPanel {
+public class GUIPanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 800;
     static final int SCREEN_HEIGHT = 600;
+    boolean running = false;
+    Color startingColor;
+    int delay = 120000;
+    Timer timer;
+    Random random;
+    
+    GUIPanel() {
+        int color = random.nextInt(2);
+        if(color == 0) {
+            startingColor = Color.red; 
+        }
+        else {
+            startingColor = Color.green;
+        }
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(startingColor);
+        this.setFocusable(true);
+        startSim();
+    }
+
+    public void startSim() {
+        running = true;
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        if(running) {
+            if(this.getBackground() == Color.green) {
+                this.setBackground(Color.red);
+            }
+            else {
+                this.setBackground(Color.green);
+            }
+            delay = delay / 2;
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
 }
